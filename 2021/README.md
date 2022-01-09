422d682283124378c0976c061910d21d11a631ce
Ben Greene <6477839+gren-bean@users.noreply.github.com>
2022-01-09 11:52:11 -0500

    AOC 2021 day 12 done!

--- 2021/README.md
+++ 2021/README.md
@@ -47,20 +47,19 @@ These are my notes on solving Advent of Code 2021. The challenges were only comp
     1. https://en.wikipedia.org/wiki/Central_tendency
     2. https://en.wikipedia.org/wiki/Median
     3. https://en.wikipedia.org/wiki/Geometric_median
-    4. https://rcoh.me/posts/linear-time-median-finding/ - If we wanted to find median in linear time
+    4. https://rcoh.me/posts/linear-time-median-finding/ - If we wanted to find median in linear time, but this was not implemented.
     5. https://en.wikipedia.org/wiki/Arithmetic_progression
     6. https://en.wikipedia.org/wiki/Triangular_number
-    7. https://www.johnmyleswhite.com/notebook/2013/03/22/modes-medians-and-means-an-unifying-perspective/
-        - Excellent reference on representing a dataset's *Central Tendency*
+    7. https://www.johnmyleswhite.com/notebook/2013/03/22/modes-medians-and-means-an-unifying-perspective/ - This is an excellent reference on representing a dataset's *Central Tendency*
 
 ### Day 8
 - **Part1**: Simple warm up, really just meant to get you familiar with data format.
 - **Part2**: Required analyzing the specifics of the problem to come up with how to map each combination for digits that could not be uniquely identified by their signal length. The core strategy was:
-    1. Define the unique "position signatures" for the number signals of lengths 5 and 6.
-    2. Use the easily spotted digits of 1, 4, and 7 to narrow the decoding search space (for character position mappings) down to 8 possible mappings per signal (from an original of 5040 combinations). Given the small input, a more brute force approach could have skipped this first step, but narrowing the search space is likely a better way to do it.
+    1. Define the unique "position signatures" for the number signals of lengths 5 and 6 (which corresponded to `2|3|5|6|9`) .
+    2. Use the easily spotted digits of `1`, `4`, and `7` to narrow the decoding search space (for character position mappings) down to 8 possible mappings per signal (from an original of 5040 combinations). Given the small input, a more brute force approach could have skipped this first step, but narrowing the search space is more efficient by a factor of x100 or more.
     3. For each signal, iterate through the 8 possible decodings to find the unique solution.
     4. Apply that solution to decode the output.
-- *Note: Code for part 2 is quite messy! Tried to take advantage of python sets, but sorted lists were also a benefit*
+- *Note: logic for part 2 is a bit messy! Tried to take advantage of python sets, but sorted lists were also a benefit*
 
 ### Day 9
 - **Part1**: Representing the 2-D grid as an adjaceny list graph made searching for the low points much easier. The lowest points are simply the points where all neighboring nodes are higher value.
@@ -72,3 +71,9 @@ These are my notes on solving Advent of Code 2021. The challenges were only comp
 ### Day 11
 - Maintain the 10 x 10 grid of "octopuses" as nodes in a connected graph (similar to day 9)
 - For each step, all octopuses increase energy level by 1. From there, used an adaption of BFS to enact impact on neighboring nodes
+
+### Day 12
+- **Part1**: First explicit graph problem! Objective is to find every simple path in the graph, with the caveat that "big caves" can be visited more than once in a path. We can do this with **Depth-First Search (DFS)** implemented with **Back-Tracking**.
+- **Part2**: Additional complexity is added by allowing **one** small cave to be visited **twice** within each path. This is solved by updating the node 'visited' attribute to an integer in order to track number of visits, and a flag variable `vtf` to track whether a small cave has been visited twice or not along the current path.
+
+### Day 13
--- 2021/day12/code.py
+++ 2021/day12/code.py
@@ -1,40 +1,127 @@
 """
-Advent of Code 2021 Problem X
+Advent of Code 2021 Problem 12
 
-Part1: TBD
-Part2: TBD
+Part1: Find every simple path through the caves, allowing multiple visits to big caves
+Part2: Same as part1, but each path can have up to 1 small cave visited twice
 """
 
 def read_input(filename, verbose=False):
-	"""
-	Read in Input
+    """
+    Read and Process Input
+    """ 
+    data = []
+    with open(filename,"r") as f:
+        lines = f.readlines()
 
-	Requires:
-	- file to be in current working directory
-	"""	
-	data = []
-	with open(filename,"r") as f:
-		lines = f.readlines()
-	# Process input
-	for line in lines:
-		data.append(line)
-	return data
+    # Process input
+    for line in lines:
+        data.append(line.strip())
+    return data
 
 
-def part1(verbose=False):
-	input_file = "input.txt"
-	data = read_input(input_file, verbose=verbose)
-	# Do stuff
-	pass
+class cave_graph():
+
+    def __init__(self, data, verbose=False):
+        """
+        Set up object representation of cave
+
+        :param data: list of connected nodes
+        """
+        self.verbose = verbose # Controls verbosity/debugging
+        self.c_graph = {}  # Graph of cave
+        self.path = []     # Tracks path during search of graph
+        self.num_paths = 0 # Number of paths discovered
+        self.vtf = True       # Visited twice flag, for part2 of problem
+        # Build Graph, input only contains unique node-pairs
+        for d in data:
+            nodes = d.split('-')
+            # Update graph with both nodes
+            if nodes[0] not in self.c_graph:
+                self.c_graph[nodes[0]] = {
+                    'v':0,  # visited or not
+                    'nbrs': [nodes[1]],  # neighbor nodes
+                    'big_cave': nodes[0].isupper()  # If it's a big cave or not
+                }
+            else:
+                 self.c_graph[nodes[0]]['nbrs'].append(nodes[1])
+            if nodes[1] not in self.c_graph:
+                self.c_graph[nodes[1]] = {
+                    'v':0,  # visited or not
+                    'nbrs': [nodes[0]],  # neighbor nodes
+                    'big_cave': nodes[1].isupper()  # If it's a big cave or not
+                }
+            else:
+                 self.c_graph[nodes[1]]['nbrs'].append(nodes[0])
+
+    def __str__(self):
+        output = ''
+        for u in self.c_graph:
+            output += (f"{u} -\n\tNeighbors: {self.c_graph[u]['nbrs']}"
+                f"\n\tVisited: {self.c_graph[u]['v']}\n")
+        return output
 
 
-def part2(verbose=False):
-	input_file = "input.txt"
-	data = read_input(input_file, verbose=verbose)
-	# Do stuff
-	pass
+    def dfs_explore(self, v):
+        """
+        Recursive DFS exploration of graph
+        
+        :param v: (string) starting node
+        :returns: True/False on if path was found
+        """
+
+        # Check if path was found
+        if v == 'end':
+            self.num_paths += 1
+            if self.verbose: print(f"{','.join(self.path)},end")
+            return True
+
+        # Otherwise, continue exploring until dead end
+        self.path.append(v)  # Update path
+        # Mark node visited if it is NOT a big cave
+        if not self.c_graph[v]['big_cave']:
+            self.c_graph[v]['v'] += 1
+
+        path_found = False
+        for u in self.c_graph[v]['nbrs']:
+            # If not yet visited, explore node
+            if self.c_graph[u]['v'] == 0:
+                if self.dfs_explore(u): path_found = True
+            
+            # [Part2] if (small cave) has been visited, but we have
+            # Not yet visited a small cave twice, we can still
+            # Explore it
+            elif self.c_graph[u]['v'] == 1 and not self.vtf:
+                self.vtf = True  # Flag that a small cave was visited twice
+                if self.dfs_explore(u): path_found = True
+        
+        # Back-tracking component - mark node unvisited before returning
+        if self.c_graph[v]['v'] == 2: self.vtf = False  # reset flag
+        if self.c_graph[v]['v'] > 0: self.c_graph[v]['v'] -= 1 
+        self.path.pop()
+        return path_found
+
+
+    def find_all_paths(self):
+        """Finds all paths from start -> end given certain constraints"""
+        # Reset graph
+        self.num_paths = 0
+        self.path = []
+        self.c_graph['start']['v'] = 1
+        
+        # Explore all paths
+        self.dfs_explore('start')
+        return self.num_paths
+
+
+def solution(verbose=False):
+    input_file = "input.txt"
+    data = read_input(input_file, verbose=verbose)
+    
+    c = cave_graph(data, verbose=verbose)
+    print(f'part1 answer: {c.find_all_paths()}')
+    c.vtf = False
+    print(f'part2 answer: {c.find_all_paths()}')
 
 
 if __name__ == "__main__":
-    part1(verbose=False)
-    part2(verbose=False)
+    solution(verbose=False)
